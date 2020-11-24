90d606c4e316d2269200fe6086337ba8cb4fed16
Massimiliano Ambrogio <maxi.ambrogio@gmail.com>
2020-11-24 18:24:34 +0100

    Project 36 parte 1.0 add and fix html css js

---
+++ js/main.js
@@ -0,0 +1,51 @@
+/**
+ * Attraverso una chiamata AJAX all’API di boolean 
+ * avremo a disposizione una decina di dischi musicali. 
+   Utilizzare Postman per analizzare i dati 
+   restituiti dall'API, ancora prima di scrivere codice.
+   Utilizzando Vue stampiamo tutto a schermo. 
+   Creare poi una select con i seguenti generi: 
+   pop, rock, metal e jazz. 
+   In base a cosa scegliamo nella select 
+   vedremo i corrispondenti cd.
+ */
+
+// Parte Vue 
+const app = new Vue({
+    el: '#app',
+    data: {
+        cds: [],
+        actualGenre: 'all'
+    },
+    created() {
+        /**
+         * Get all albums, filters
+         */
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then(result => {
+                console.log(result.data);
+
+                this.cds = result.data.response;
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    },
+    methods: {
+        filterGenre() {
+            axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then(result => {
+                let cdsList = result.data.response;
+
+                if (this.actualGenre !== 'all') {
+                    cdsList = cdsList.filter(cd => cd.genre.tolowerCase() === this.actualGenre);
+                }
+
+                this.cds = cdsList;
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        }
+    }
+});
